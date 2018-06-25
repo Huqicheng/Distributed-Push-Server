079e65ce492eae1fcf398ee0c253caa220b8e613
胡其成 <huqicheng@huqicheng.local>
2018-06-24 20:33:46 -0400

    reconfiguration

--- example/index.js
+++ example/index.js
@@ -18,20 +18,29 @@ class MyHandler extends BasicHandler {
 
 class RedisHandler extends BasicHandler {
 	onMessageRecieved(socket, data) {
-		let msg = data;
+		console.log(data);
+		let msg = JSON.parse(data);
 		if (msg.type == "join") {
 			socket.sessionController = new SessionController(msg.user,msg.channel);
-      		socket.sessionController.subscribe(socket);
+      		socket.sessionController.subscribe(socket, function(channel, count){
+      			var joinMessage = JSON.stringify({type: 'control', user: socket.sessionController.user, msg: ' joined the channel' });
+				socket.sessionController.publish(joinMessage);
+      		});
       		return;
 		}
 
-		if (msg.type == "chat") {
+		if (msg.type == "message") {
       		if (socket.sessionController == null) {
         		// implicit login - socket can be timed out or disconnected
         		socket.sessionController = new SessionController(msg.user,msg.channel);
-        		socket.sessionController.rejoin(socket, msg);
+        		socket.sessionController.rejoin(socket, msg, function(channel, count){
+        			var rejoin = JSON.stringify({type: 'control', user: socket.sessionController.user, msg: ' rejoined the channel' });
+					socket.sessionController.publish(rejoin);
+					var reply = JSON.stringify({type: 'message', user: msg.user, msg: msg.msg });
+					socket.sessionController.publish(reply);
+        		});
       		} else {
-        		var reply = JSON.stringify({type: 'chat', user: msg.user, msg: msg.msg });
+        		var reply = JSON.stringify({type: 'message', user: msg.user, msg: msg.msg });
         		socket.sessionController.publish(reply);
       		}
       		return;
@@ -39,7 +48,7 @@ class RedisHandler extends BasicHandler {
 	}
 
 	onConnected(socket) {
-		
+		console.log(socket.id);
 	}
 
 	onDisconnected(socket) {
